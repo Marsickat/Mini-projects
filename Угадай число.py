a73f64d686929f9d16fb6394951a4d5866ce5427
Marsickat <ruslan.marsikcat@yandex.ru>
2022-08-19 15:46:31 +0300

    v2.0
    1. При угадывании числа теперь показывает сообщение, где написано сколько потребовалось попыток
    2. При угадывании числа теперь можно перезапустить игру, ответив "да" на соответствующий вопрос
    3. В начале теперь задаётся вопрос, позволяющий самостоятельно определить границу для загадывания числа (раньше по умолчанию было 100)

--- Угадай число.py
+++ Угадай число.py
@@ -1,25 +1,43 @@
 import random
 
 
-def is_valid(value):
-    return value.isdigit() and 1 <= int(value) <= 100
+def is_valid(value, right):
+    return value.isdigit() and 1 <= int(value) <= int(right)
 
 
-num = random.randint(1, 100)
-print("Добро пожаловать в числовую угадайку!\n\nВведите целое число от 1 до 100:")
+def game():
+    right = input("Введите целое число, от 1 до которого оно будет загадано:\n")
+    while not right.isdigit():
+        right = input("А может быть всё-таки введём целое число:\n")
+    right = int(right)
+    num = random.randint(1, right)
+    attempts = 0
+    print(f"Введите число от 1 до {right}:")
+    while True:
+        digit = input()
+        if is_valid(digit, right):
+            digit = int(digit)
+            if digit < num:
+                print("Ваше число меньше загаданного, попробуйте еще разок")
+                attempts += 1
+            elif digit > num:
+                print("Ваше число больше загаданного, попробуйте еще разок")
+                attempts += 1
+            elif digit == num:
+                attempts += 1
+                print(f"Вы угадали, поздравляем! Количество затраченных попыток: {attempts}")
+                again = input("Напишите \"да\", если хотите сыграть ещё раз:\n")
+                if again.lower() == "да":
+                    print("\nНачинаем новую игру!")
+                    game()
+                else:
+                    return
+        else:
+            print(f"А может быть всё-таки введём целое число от 1 до {right}?")
 
-while True:
-    digit = input()
-    if is_valid(digit):
-        digit = int(digit)
-        if digit < num:
-            print("Ваше число меньше загаданного, попробуйте еще разок")
-        elif digit > num:
-            print("Ваше число больше загаданного, попробуйте еще разок")
-        elif digit == num:
-            print(f"Вы угадали, поздравляем!")
-            break
-    else:
-        print("А может быть всё-таки введём целое число от 1 до 100?")
+
+print("Добро пожаловать в числовую угадайку!\n")
+game()
+
 
 print("Спасибо, что играли в числовую угадайку. Ещё увидимся...")
