8909a89e65688bb6062a41bfc0c8e784447f42dc
Marsickat <ruslan.marsikcat@yandex.ru>
2022-08-22 08:54:09 +0300

    v2.1
    Исправлен баг, из-за которого игра продолжалась при попытке выйти после неоднократного продолжения игры

--- Угадай число.py
+++ Угадай число.py
@@ -5,39 +5,43 @@ def is_valid(value, right):
     return value.isdigit() and 1 <= int(value) <= int(right)
 
 
-def game():
-    right = input("Введите целое число, от 1 до которого оно будет загадано:\n")
-    while not right.isdigit():
-        right = input("А может быть всё-таки введём целое число:\n")
-    right = int(right)
-    num = random.randint(1, right)
-    attempts = 0
-    print(f"Введите число от 1 до {right}:")
-    while True:
-        digit = input()
-        if is_valid(digit, right):
-            digit = int(digit)
-            if digit < num:
-                print("Ваше число меньше загаданного, попробуйте еще разок")
-                attempts += 1
-            elif digit > num:
-                print("Ваше число больше загаданного, попробуйте еще разок")
-                attempts += 1
-            elif digit == num:
-                attempts += 1
-                print(f"Вы угадали, поздравляем! Количество затраченных попыток: {attempts}")
-                again = input("Напишите \"да\", если хотите сыграть ещё раз:\n")
-                if again.lower() == "да":
-                    print("\nНачинаем новую игру!")
-                    game()
-                else:
-                    return
-        else:
-            print(f"А может быть всё-таки введём целое число от 1 до {right}?")
+right = input("Введите целое число, от 1 до которого оно будет загадано:\n")
+while not right.isdigit():
+    right = input("А может быть всё-таки введём целое число:\n")
+right = int(right)
+print(f"Введите число от 1 до {right}:")
+num = random.randint(1, right)
+attempts = 0
+while True:
+    digit = input()
+    if is_valid(digit, right):
+        digit = int(digit)
+        if digit < num:
+            print("Ваше число меньше загаданного, попробуйте еще разок")
+            attempts += 1
+        elif digit > num:
+            print("Ваше число больше загаданного, попробуйте еще разок")
+            attempts += 1
+        elif digit == num:
+            attempts += 1
+            print(f"Вы угадали, поздравляем! Количество затраченных попыток: {attempts}")
+            again = input("Напишите \"да\", если хотите сыграть ещё раз:\n")
+            if again.lower() == "да":
+                print("\nНачинаем новую игру!")
+                right = input("Введите целое число, от 1 до которого оно будет загадано:\n")
+                while not right.isdigit():
+                    right = input("А может быть всё-таки введём целое число:\n")
+                right = int(right)
+                print(f"Введите число от 1 до {right}:")
+                num = random.randint(1, right)
+                attempts = 0
+                continue
+            else:
+                break
+    else:
+        print(f"А может быть всё-таки введём целое число от 1 до {right}?")
 
 
 print("Добро пожаловать в числовую угадайку!\n")
-game()
-
 
 print("Спасибо, что играли в числовую угадайку. Ещё увидимся...")
